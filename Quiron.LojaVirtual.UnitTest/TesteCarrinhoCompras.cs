d92289e457293247481cf71a93df1910dd8b0365
marcosrseki <marcos.seki@yahoo.com.br>
2015-04-20 11:58:45 -0300

    Aula17

--- Quiron.LojaVirtual.UnitTest/TesteCarrinhoCompras.cs
+++ Quiron.LojaVirtual.UnitTest/TesteCarrinhoCompras.cs
@@ -42,5 +42,138 @@ namespace Quiron.LojaVirtual.UnitTest
             Assert.AreEqual(itens[0].Produto, produto1);
             Assert.AreEqual(itens[1].Produto, produto2);
         }
+
+        [TestMethod]
+        public void AdicionarProdutoExistenteParaCarrinho()
+        {
+            //Arrange - Criacao dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+            carrinho.AdicionarItem(produto1, 10);
+
+            //Act
+            ItemCarrinho[] resultado = carrinho.ItensCarrinho.OrderBy(c => c.Produto.ProdutoId).ToArray();
+
+            //Assert
+            Assert.AreEqual(resultado.Length, 2);
+
+            Assert.AreEqual(resultado[0].Quantidade, 11);
+
+            Assert.AreEqual(resultado[1].Quantidade, 1);
+        }
+
+        [TestMethod]
+        public void RemoverItensCarrinho()
+        {
+            //Arrange - Criacao dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1"
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2"
+            };
+
+            Produto produto3 = new Produto
+            {
+                ProdutoId = 3,
+                Nome = "Teste 3"
+            };
+
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 3);
+            carrinho.AdicionarItem(produto3, 5);
+            carrinho.AdicionarItem(produto2, 1);
+
+            carrinho.RemoverItem(produto2);
+
+            Assert.AreEqual(carrinho.ItensCarrinho.Where(c=>c.Produto == produto2).Count(), 0);
+
+            Assert.AreEqual(carrinho.ItensCarrinho.Count(), 2);
+        }
+
+        [TestMethod]
+        public void CalcularValorTotal()
+        {
+            //Arrange - Criacao dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+                Preco = 100M
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+                Preco = 50M
+            };
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+            carrinho.AdicionarItem(produto1, 3);
+
+            decimal resultado = carrinho.ObterValorTotal();
+
+            Assert.AreEqual(resultado, 450M);
+        }
+
+        [TestMethod]
+        public void LimparItensCarrinho()
+        {
+            //Arrange - Criacao dos produtos
+            Produto produto1 = new Produto
+            {
+                ProdutoId = 1,
+                Nome = "Teste 1",
+                Preco = 100M
+            };
+
+            Produto produto2 = new Produto
+            {
+                ProdutoId = 2,
+                Nome = "Teste 2",
+                Preco = 50M
+            };
+
+            //Arrange
+            Carrinho carrinho = new Carrinho();
+            carrinho.AdicionarItem(produto1, 1);
+            carrinho.AdicionarItem(produto2, 1);
+            carrinho.AdicionarItem(produto1, 3);
+
+            carrinho.LimparCarrinho();
+
+            //Assert
+            Assert.AreEqual(carrinho.ItensCarrinho.Count(), 0);
+
+
+            
+        }
     }
 }
