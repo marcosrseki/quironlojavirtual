abddc41d5c5f973bdf840956038a8ccab7efb51c
marcosrseki <marcos.seki@yahoo.com.br>
2015-04-27 16:53:43 -0300

    Aula29 - Cria a view da parte administrativa do cadastro de produtos.

--- Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
 
 namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
 {
     public class ProdutoController : Controller
     {
+        private ProdutosRepositorio _repositorio;
+
         // GET: Administrativo/Produto
         public ActionResult Index()
         {
-            return View();
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos;
+            return View(produtos);
         }
     }
 }
